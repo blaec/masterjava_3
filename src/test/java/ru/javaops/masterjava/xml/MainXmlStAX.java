aa11e3ada0ed7914707d98f2a846d82b37b38fa4
blaec <odeskonst@yandex.ru>
2019-02-18 22:26:49 +0200

    h_02_05_00 display StAX result in html table

--- src/test/java/ru/javaops/masterjava/xml/MainXmlStAX.java
+++ src/test/java/ru/javaops/masterjava/xml/MainXmlStAX.java
@@ -5,12 +5,15 @@ import ru.javaops.masterjava.xml.util.StaxStreamProcessor;
 
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.XMLEvent;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
 public class MainXmlStAX {
     public static void main(String[] args) throws IOException, XMLStreamException {
         String project = "topjava";
+        final String DELIMITER = "-";
 
         String projectId = null;
         try (StaxStreamProcessor processor =
@@ -45,12 +48,33 @@ public class MainXmlStAX {
                 String userGroups = processor.getReader().getAttributeValue(3);
                 if ((userName = processor.getElementValue("fullName")) != null) {
                     if (groups.stream().anyMatch(userGroups::contains)) {
-                        userEmail.add(userName + "\\" + email);
+                        userEmail.add(userName + DELIMITER + email);
                     }
                 }
             }
         }
         Collections.sort(userEmail);
         System.out.println(userEmail);
+
+        final String HTML_START = "<!DOCTYPE html><head></head><body><table border=\"1\"><tr><th>User</th><th>e-mail</th></tr>";
+        final String HTML_END = "</table></body></html>";
+        final String OPEN_TD = "<td>";
+        final String OPEN_TR = "<tr>";
+        final String CLOSE_TD = "</td>";
+        final String CLOSE_TR = "</tr>";
+        StringBuffer html = new StringBuffer();
+        html.append(HTML_START);
+        for (String user : userEmail) {
+            String[] split = user.split(DELIMITER);
+            html.append(OPEN_TR)
+                    .append(OPEN_TD).append(split[0]).append(CLOSE_TD)
+                    .append(OPEN_TD).append(split[1]).append(CLOSE_TD)
+                    .append(CLOSE_TR);
+        }
+        html.append(HTML_END);
+        FileWriter fileWriter = new FileWriter(new File("src/test/resources/output.html"));
+        fileWriter.write(html.toString());
+        fileWriter.close();
+        System.out.println(html.toString());
     }
 }
