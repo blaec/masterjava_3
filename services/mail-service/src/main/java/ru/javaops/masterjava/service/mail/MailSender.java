0b727ce6e0601baa7d432028a6b7eee26e06f0f4
blaec <odeskonst@yandex.ru>
2019-03-25 23:12:05 +0200

    h_06_01_01 send email via client or soap

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
@@ -1,12 +1,34 @@
 package ru.javaops.masterjava.service.mail;
 
+import com.typesafe.config.Config;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import ru.javaops.masterjava.config.Configs;
 
 import java.util.List;
 
 @Slf4j
 public class MailSender {
     static void sendMail(List<Addressee> to, List<Addressee> cc, String subject, String body) {
-        log.info("Send mail to \'" + to + "\' cc \'" + cc + "\' subject \'" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
+        Config mail = Configs.getConfig("mail.conf","mail");
+
+        Email email = new SimpleEmail();
+        email.setHostName(mail.getString("host"));
+        email.setSmtpPort(mail.getInt("port"));
+        email.setAuthenticator(new DefaultAuthenticator(mail.getString("username"), mail.getString("password")));
+        email.setSSLOnConnect(mail.getBoolean("useSSL"));
+        email.setSubject(subject);
+        try {
+            email.setFrom(mail.getString("fromName"));
+            email.setMsg(body);
+            email.addTo(to.get(0).getEmail(), to.get(0).getName());
+            email.send();
+            log.info("Sent mail to \'" + to + "\' cc \'" + cc + "\' subject \'" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
+        } catch (EmailException e) {
+            e.printStackTrace();
+        }
     }
 }
