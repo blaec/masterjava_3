647bc67c4e7749dd413d8a1df631fe6165479992
blaec <odeskonst@yandex.ru>
2019-04-24 22:10:56 +0300

    h_07_03_01 simple interface of sending emails to selected users

--- web/webapp/src/main/java/ru/javaops/masterjava/webapp/UsersServlet.java
+++ web/webapp/src/main/java/ru/javaops/masterjava/webapp/UsersServlet.java
@@ -1,9 +1,12 @@
 package ru.javaops.masterjava.webapp;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import org.thymeleaf.context.WebContext;
 import ru.javaops.masterjava.persist.DBIProvider;
 import ru.javaops.masterjava.persist.dao.UserDao;
+import ru.javaops.masterjava.service.mail.Addressee;
+import ru.javaops.masterjava.service.mail.MailWSClient;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 import static ru.javaops.masterjava.common.web.ThymeleafListener.engine;
 
@@ -24,4 +28,14 @@ public class UsersServlet extends HttpServlet {
                 ImmutableMap.of("users", userDao.getWithLimit(20)));
         engine.process("users", webContext, resp.getWriter());
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        MailWSClient.sendToGroup(
+                Arrays.stream(req.getParameterValues("sendTo")).map(Addressee::new).collect(ImmutableSet.toImmutableSet()),
+                ImmutableSet.of(new Addressee("Copy <odeskonst@yandex.ru>")),
+                req.getParameter("subject"),
+                req.getParameter("body"));
+    }
+
 }
